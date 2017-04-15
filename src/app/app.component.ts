2f3e04205f488a3af5e09faeb27d300f5ba89327
pradeepp2008 <ppradeep2008@gmail.com>
2017-04-16 01:52:50 +0800

    foodbees

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  title = 'app works!';
+}
